385bf18351f3362840bd889fc0ee088b7fe4fc6b
tung vo <vomtung@gmail.com>
2019-07-28 19:48:33 +0700

    change package

--- src/main/java/com/example/kafka/producer/example/SpringBootProducerExampleApplication.java
+++ src/main/java/com/example/kafka/producer/example/SpringBootProducerExampleApplication.java
@@ -1,4 +1,4 @@
-package com.example.kafka.producer.springbootproducerexample;
+package com.example.kafka.producer.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
