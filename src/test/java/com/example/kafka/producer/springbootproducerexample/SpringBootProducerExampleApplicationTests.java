b0602cafb00a4b48d2920194b9cb6ae165dc6e3c
tung vo <vomtung@gmail.com>
2019-07-28 17:25:58 +0700

    init project

---
+++ src/test/java/com/example/kafka/producer/springbootproducerexample/SpringBootProducerExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.kafka.producer.springbootproducerexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootProducerExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
